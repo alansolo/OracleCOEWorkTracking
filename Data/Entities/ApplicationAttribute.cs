033802ef4ddd59aac39966235b5d879990efc961
Alan Michel Solorzano Tapia <K697344@nac.ppg.com>
2020-10-30 13:27:29 -0600

    Agregar archivos de proyecto.

---
+++ Data/Entities/ApplicationAttribute.cs
@@ -0,0 +1,36 @@
+﻿using OracleCOEWorkTracking.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OracleCOEWorkTracking.Data.Entities
+{
+    public class ApplicationAttribute : ITrack
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        // ITrack
+        [MaxLength(50)]
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        [MaxLength(50)]
+        public string ModifiedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public bool DeleteMark { get; set; }
+        public int ApplicationId { get; set; }
+        public string Attribute1 { get; set; }
+        public string Attribute2 { get; set; }
+        public string Attribute3 { get; set; }
+        public string Attribute4 { get; set; }
+        public string Attribute5 { get; set; }
+        public string Attribute6 { get; set; }
+        public string Attribute7 { get; set; }
+        public string Attribute8 { get; set; }
+        public string Attribute9 { get; set; }
+        public string Attribute10 { get; set; }
+    }
+}
+
